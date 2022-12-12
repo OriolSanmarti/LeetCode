508fe9f5855bd10b077a5e21863f6e85f9e7a1fd
OriolSanmarti <109756173+OriolSanmarti@users.noreply.github.com>
2022-12-12 19:32:56 +0100

    Add files via upload

---
+++ LinkedList/FindMid.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+}head;
+
+class Linkedlist {
+public:
+    ListNode* head;
+ 
+public:
+    // Default constructor
+    Linkedlist() { head = NULL; }
+ 
+    // Function to insert a
+    // node at the end of the
+    // linked list.
+    void insertNode(int);
+ 
+    // Function to print the
+    // linked list.
+    void printList();
+ 
+    // Function to delete the
+    // node at given position
+    void deleteNode(int);
+};
+
+// Function to insert a new node.
+void Linkedlist::insertNode(int data)
+{
+    // Create the new Node.
+    ListNode* newNode = new ListNode(data);
+ 
+    // Assign to head
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+ 
+    // Traverse till end of list
+    ListNode* temp = head;
+    while (temp->next != NULL) {
+ 
+        // Update temp
+        temp = temp->next;
+    }
+ 
+    // Insert at the last.
+    temp->next = newNode;
+}
+
+class Solution {
+public:
+    void printList(ListNode* head) {
+        ListNode* temp = head;
+ 
+	    // Check for empty list.
+	    if (head == NULL) {
+	        cout << "List empty" << endl;
+	        return;
+	    }
+	 
+	    // Traverse the list.
+	    while (temp != NULL) {
+	        cout << temp->val << " ";
+	        temp = temp->next;
+	    }
+    }
+    ListNode* middleNode(ListNode* &head) {
+        ListNode* l1 = head;
+        ListNode* l2 = head;
+        
+        while(l2 != NULL and l2->next != NULL)
+        {
+            l1 = l1->next;
+            l2 = l2->next->next;
+        }
+        head = l1;
+        return l1;
+    }
+};
+
+
+int main(){
+	
+	Linkedlist list;
+	list.insertNode(1);
+    list.insertNode(2);
+    list.insertNode(3);
+    list.insertNode(4);
+    list.insertNode(5);
+    
+ 	Solution sol;
+ 	sol.middleNode(list.head);
+	sol.printList(list.head);
+	
+}
